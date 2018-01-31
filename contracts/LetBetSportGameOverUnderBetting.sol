91f7f829ab470e4e8ba935b51525b5122832e007
jamesdave <tnt@let.bet>
2018-01-31 16:21:32 +0700

    Update sport game over-under

--- contracts/LetBetSportGameOverUnderBetting.sol
+++ contracts/LetBetSportGameOverUnderBetting.sol
@@ -17,6 +17,7 @@ contract LetBetSportGameOverUnderBetting is Ownable {
         OUBetOption betOption;
         uint256 betAmount;
         uint256 winAmount;
+        uint256 betTimestamp;
         
         uint16[2] ouRate;
         uint16[2] winRateOver;
@@ -30,6 +31,7 @@ contract LetBetSportGameOverUnderBetting is Ownable {
         GameState status;
         uint256 betLimit; // 0: no limit
         uint256 betTotalLimit; // 0: no limit
+        uint256 beginTimestamp;
         
         uint16[2][] ouRate;
         uint16[2][] winRateOver;
@@ -48,38 +50,39 @@ contract LetBetSportGameOverUnderBetting is Ownable {
     
     event RefundAll(uint256 gameId);
     event PerformOUBet(uint256 gameId, uint16[2] gameScore);
-    event BetWithOUDealer(address player, uint256 gameId, uint8 option, uint256 amount, uint16[2] ouRate, uint16[2] winRateOver, uint16[2] winRateUnder);
+    event BetWithOUDealer(address player, uint256 gameId, uint8 option, uint256 amount, uint256 betTimestamp, uint16[2] ouRate, uint16[2] winRateOver, uint16[2] winRateUnder);
     
     function LetBetSportGameOverUnderBetting(address _creditManager) public {
         creditManager = _creditManager;
     }
     
-    function getInfo(uint256 _gameId) public constant returns (uint16[2] gameScore, uint8 result, uint8 status, uint256 betLimit, uint256 betTotalLimit, uint16[2][] ouRate, uint16[2][] winRateOver, uint16[2][] winRateUnder, uint numOfOUBetPlayers, uint256 ouBetTotal) {
+    function getInfo(uint256 _gameId) public constant returns (uint16[2] gameScore, uint8 result, uint8 status, uint256 betLimit, uint256 betTotalLimit, uint256 beginTimestamp, uint16[2][] ouRate, uint16[2][] winRateOver, uint16[2][] winRateUnder, uint numOfOUBetPlayers, uint256 ouBetTotal) {
         Game memory game = games[_gameId];
-        return (game.gameScore, uint8(game.result), uint8(game.status), game.betLimit, game.betTotalLimit, game.ouRate, game.winRateOver, game.winRateUnder, game.ouBetPlayers.length, game.ouBetTotal);
+        return (game.gameScore, uint8(game.result), uint8(game.status), game.betLimit, game.betTotalLimit, game.beginTimestamp, game.ouRate, game.winRateOver, game.winRateUnder, game.ouBetPlayers.length, game.ouBetTotal);
     }
     
-    function getPlayerOUBet(uint256 _gameId, uint _index) public constant returns (address addr, uint8 betOption, uint256 betAmount, uint256 winAmount, uint16[2] ouRate, uint16[2] winRateOver, uint16[2] winRateUnder) {
+    function getPlayerOUBet(uint256 _gameId, uint _index) public constant returns (address addr, uint8 betOption, uint256 betAmount, uint256 winAmount, uint256 betTimestamp, uint16[2] ouRate, uint16[2] winRateOver, uint16[2] winRateUnder) {
         require(_index < games[_gameId].ouBetPlayers.length);
         
         OUBetPlayer memory player = games[_gameId].ouBetPlayers[_index];
-        return (player.addr, uint8(player.betOption), player.betAmount, player.winAmount, player.ouRate, player.winRateOver, player.winRateUnder);
+        return (player.addr, uint8(player.betOption), player.betAmount, player.winAmount, player.betTimestamp, player.ouRate, player.winRateOver, player.winRateUnder);
     }
     
     function getGameList() public constant returns (uint256[] gameIds) {
         return gameList;
     }
     
-    function start(uint256 _gameId, uint256 _betLimit, uint256 _betTotalLimit, uint16[2][] _ouRate, uint16[2][] _winRateOver, uint16[2][] _winRateUnder) onlyOwner public returns (bool success) {
+    function start(uint256 _gameId, uint256 _betLimit, uint256 _betTotalLimit, uint256 _beginTimestamp, uint16[2][] _ouRate, uint16[2][] _winRateOver, uint16[2][] _winRateUnder) onlyOwner public returns (bool success) {
         require(games[_gameId].status != GameState.END);
         
         if (games[_gameId].status == GameState.UNKNOWN) {
-            games[_gameId].gameId        = _gameId;
-            games[_gameId].gameScore     = [0, 0];
-            games[_gameId].result        = GameResult.UNKNOWN;
-            games[_gameId].status        = GameState.IN_BET;
-            games[_gameId].betLimit      = _betLimit;
-            games[_gameId].betTotalLimit = _betTotalLimit;
+            games[_gameId].gameId         = _gameId;
+            games[_gameId].gameScore      = [0, 0];
+            games[_gameId].result         = GameResult.UNKNOWN;
+            games[_gameId].status         = GameState.IN_BET;
+            games[_gameId].betLimit       = _betLimit;
+            games[_gameId].betTotalLimit  = _betTotalLimit;
+            games[_gameId].beginTimestamp = _beginTimestamp;
             
             games[_gameId].ouRate       = _ouRate;
             games[_gameId].winRateOver  = _winRateOver;
@@ -135,43 +138,44 @@ contract LetBetSportGameOverUnderBetting is Ownable {
         int16 delta = 0;
         LetBetCreditManager lbcm = LetBetCreditManager(creditManager);
         
-        for (i = 0; i < games[_gameId].ouBetPlayers.length; ++i) {
-            ouRateNatural = games[_gameId].ouBetPlayers[i].ouRate[0] / MAGIC_NUMBER;
-            ouRateDecimal = games[_gameId].ouBetPlayers[i].ouRate[0] % MAGIC_NUMBER;
+        Game memory game = games[_gameId];
+        for (i = 0; i < game.ouBetPlayers.length; ++i) {
+            ouRateNatural = game.ouBetPlayers[i].ouRate[0] / MAGIC_NUMBER;
+            ouRateDecimal = game.ouBetPlayers[i].ouRate[0] % MAGIC_NUMBER;
             if (ouRateDecimal == 3) { // over/under: 0.5/1, 1.5/2, 2.5/3,...
                 ouRateNatural = ouRateNatural + 1;
             }
             delta = int16(_gameScore[0]) + int16(_gameScore[1]) - int16(ouRateNatural);
             amount = 0;
-            betAmount = games[_gameId].ouBetPlayers[i].betAmount;
+            betAmount = game.ouBetPlayers[i].betAmount;
             
-            if (delta > 0 && games[_gameId].ouBetPlayers[i].betOption == OUBetOption.OVER) { // over win -> bet over win
-                amount = betAmount.add(betAmount.mul(games[_gameId].ouBetPlayers[i].winRateOver[0]).div(games[_gameId].ouBetPlayers[i].winRateOver[1]));
-            } else if (delta < 0 && games[_gameId].ouBetPlayers[i].betOption == OUBetOption.UNDER) { // under win -> bet under win
-                amount = betAmount.add(betAmount.mul(games[_gameId].ouBetPlayers[i].winRateUnder[0]).div(games[_gameId].ouBetPlayers[i].winRateUnder[1]));
+            if (delta > 0 && game.ouBetPlayers[i].betOption == OUBetOption.OVER) { // over win -> bet over win
+                amount = betAmount.mul(game.ouBetPlayers[i].winRateOver[0]).div(game.ouBetPlayers[i].winRateOver[1]);
+            } else if (delta < 0 && game.ouBetPlayers[i].betOption == OUBetOption.UNDER) { // under win -> bet under win
+                amount = betAmount.mul(game.ouBetPlayers[i].winRateUnder[0]).div(game.ouBetPlayers[i].winRateUnder[1]);
             } else if (delta == 0) { // draw
                 if (ouRateDecimal == 0) { // over/under: 1, 2, 3,... -> stake refund
                     amount = betAmount;
-                } else if (ouRateDecimal == 2 && games[_gameId].ouBetPlayers[i].betOption == OUBetOption.UNDER) { // over/under: 0.5, 1.5, 2.5,... -> bet under win
-                    amount = betAmount.add(betAmount.mul(games[_gameId].ouBetPlayers[i].winRateUnder[0]).div(games[_gameId].ouBetPlayers[i].winRateUnder[1]));
+                } else if (ouRateDecimal == 2 && game.ouBetPlayers[i].betOption == OUBetOption.UNDER) { // over/under: 0.5, 1.5, 2.5,... -> bet under win
+                    amount = betAmount.mul(game.ouBetPlayers[i].winRateUnder[0]).div(game.ouBetPlayers[i].winRateUnder[1]);
                 } else if (ouRateDecimal == 1) { // over/under: 1/1.5, 2/2.5, 3/3.5,... -> bet over half lose, bet under half win
-                    if (games[_gameId].ouBetPlayers[i].betOption == OUBetOption.OVER) {
+                    if (game.ouBetPlayers[i].betOption == OUBetOption.OVER) {
                         amount = betAmount.div(2);
-                    } else if (games[_gameId].ouBetPlayers[i].betOption == OUBetOption.UNDER) {
-                        amount = betAmount.add(betAmount.mul(games[_gameId].ouBetPlayers[i].winRateUnder[0]).div(games[_gameId].ouBetPlayers[i].winRateUnder[1] * 2));
+                    } else if (game.ouBetPlayers[i].betOption == OUBetOption.UNDER) {
+                        amount = betAmount.add(betAmount.mul(game.ouBetPlayers[i].winRateUnder[0] - game.ouBetPlayers[i].winRateUnder[1]).div(game.ouBetPlayers[i].winRateUnder[1] * 2));
                     }
                 } else if (ouRateDecimal == 3) { // over/under: 0.5/1, 1.5/2, 2.5/3,... -> bet over half win, bet under half lose
-                    if (games[_gameId].ouBetPlayers[i].betOption == OUBetOption.OVER) {
-                        amount = betAmount.add(betAmount.mul(games[_gameId].ouBetPlayers[i].winRateOver[0]).div(games[_gameId].ouBetPlayers[i].winRateOver[1] * 2));
-                    } else if (games[_gameId].ouBetPlayers[i].betOption == OUBetOption.UNDER) {
+                    if (game.ouBetPlayers[i].betOption == OUBetOption.OVER) {
+                        amount = betAmount.add(betAmount.mul(game.ouBetPlayers[i].winRateOver[0] - game.ouBetPlayers[i].winRateOver[1]).div(game.ouBetPlayers[i].winRateOver[1] * 2));
+                    } else if (game.ouBetPlayers[i].betOption == OUBetOption.UNDER) {
                         amount = betAmount.div(2);
                     }
                 }
             }
             
             if (amount > 0) {
-                if (!lbcm.increaseCredit(games[_gameId].ouBetPlayers[i].addr, amount)) revert();
-                games[_gameId].ouBetPlayers[i].winAmount = amount;
+                if (!lbcm.increaseCredit(game.ouBetPlayers[i].addr, amount)) revert();
+                game.ouBetPlayers[i].winAmount = amount;
             }
         }
         
@@ -184,6 +188,14 @@ contract LetBetSportGameOverUnderBetting is Ownable {
         require(games[_gameId].status == GameState.IN_BET);
         
         games[_gameId].status = GameState.IN_GAME;
+        
+        return true;
+    }
+    
+    function setBeginTimestamp(uint256 _gameId, uint256 _beginTimestamp) onlyOwner public returns (bool success) {
+        require(games[_gameId].status == GameState.IN_BET);
+        
+        games[_gameId].beginTimestamp = _beginTimestamp;
         
         return true;
     }
@@ -210,6 +222,7 @@ contract LetBetSportGameOverUnderBetting is Ownable {
     
     function betWithOUDealer(uint256 _gameId, uint8 _betOption, uint256 _betAmount, uint _ouRateIndex) public returns (bool success) {
         require(games[_gameId].status == GameState.IN_BET);
+        require(now < games[_gameId].beginTimestamp);
         require(games[_gameId].ouRate.length == games[_gameId].winRateOver.length && games[_gameId].ouRate.length == games[_gameId].winRateUnder.length);
         require(_ouRateIndex < games[_gameId].ouRate.length && games[_gameId].ouRate[_ouRateIndex][1] == MAGIC_NUMBER && games[_gameId].winRateOver[_ouRateIndex][1] > 0 && games[_gameId].winRateUnder[_ouRateIndex][1] > 0);
         require(_betOption >= uint8(OUBetOption.OVER) && _betOption <= uint8(OUBetOption.UNDER));
@@ -219,15 +232,14 @@ contract LetBetSportGameOverUnderBetting is Ownable {
         
         LetBetCreditManager lbcm = LetBetCreditManager(creditManager);
         
-        uint256 currentCredit = lbcm.getCredit(msg.sender);
-        if (currentCredit < _betAmount) revert();
+        if (lbcm.getCredit(msg.sender) < _betAmount) revert();
         
         if (!lbcm.decreaseCredit(msg.sender, _betAmount)) revert();
         
-        games[_gameId].ouBetPlayers.push(OUBetPlayer(msg.sender, OUBetOption(_betOption), _betAmount, 0, games[_gameId].ouRate[_ouRateIndex], games[_gameId].winRateOver[_ouRateIndex], games[_gameId].winRateUnder[_ouRateIndex]));
+        games[_gameId].ouBetPlayers.push(OUBetPlayer(msg.sender, OUBetOption(_betOption), _betAmount, 0, now, games[_gameId].ouRate[_ouRateIndex], games[_gameId].winRateOver[_ouRateIndex], games[_gameId].winRateUnder[_ouRateIndex]));
         games[_gameId].ouBetTotal = games[_gameId].ouBetTotal.add(_betAmount);
         
-        BetWithOUDealer(msg.sender, _gameId, _betOption, _betAmount, games[_gameId].ouRate[_ouRateIndex], games[_gameId].winRateOver[_ouRateIndex], games[_gameId].winRateUnder[_ouRateIndex]);
+        BetWithOUDealer(msg.sender, _gameId, _betOption, _betAmount, now, games[_gameId].ouRate[_ouRateIndex], games[_gameId].winRateOver[_ouRateIndex], games[_gameId].winRateUnder[_ouRateIndex]);
         
         return true;
     }
